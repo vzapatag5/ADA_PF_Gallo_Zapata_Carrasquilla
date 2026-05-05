50a3d38783a71fc219258fb5aa38f6c175fe68bd
Sofia Gallo <sgallo@DESKTOP-8QV6EVD.localdomain>
2026-05-05 17:51:16 -0500

    Implementación algoritmo de Kruskal con Union-Find

---
+++ src/kruskal.hpp
@@ -0,0 +1,10 @@
+#ifndef KRUSKAL_HPP
+#define KRUSKAL_HPP
+
+#include <vector>
+#include "graph.hpp"
+
+// Algoritmo de Kruskal: recibe un grafo y devuelve el MST
+std::vector<Edge> kruskal(Graph& g);
+
+#endif
